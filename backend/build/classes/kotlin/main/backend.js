b079e1ee94842c6dbf4737f99ae3931b649535c9
Zachary Smith <zach.smith33@gmail.com>
2019-02-10 12:28:18 -0500

    add git ignore

---
+++ backend/build/classes/kotlin/main/backend.js
@@ -0,0 +1,24 @@
+(function (_, Kotlin) {
+  'use strict';
+  var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var Unit = Kotlin.kotlin.Unit;
+  function main$lambda(req, res) {
+    res.type('text/plain');
+    return res.send('i am a beautiful butterfly');
+  }
+  function main$lambda_0() {
+    println('Listening on port 3000');
+    return Unit;
+  }
+  function main() {
+    println('Hello JavaScript!');
+    var express = require('express');
+    var app = express();
+    app.get('/', main$lambda);
+    app.listen(3000, main$lambda_0);
+  }
+  _.main = main;
+  main();
+  Kotlin.defineModule('backend', _);
+  return _;
+}(module.exports, require('kotlin')));
