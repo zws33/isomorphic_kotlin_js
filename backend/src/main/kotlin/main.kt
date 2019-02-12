e0a8028820a206cdffec80b45c5ddffc21f8d28a
Zachary Smith <zach.smith33@gmail.com>
2019-02-11 19:36:15 -0500

    Add endpoint to backend project for fetching todos. Update server's static resource to serve latest front end app.

--- backend/src/main/kotlin/main.kt
+++ backend/src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
+
 external fun require(module: String): dynamic
+external val __dirname: dynamic
 
 fun main() {
     println("Hello JavaScript!")
@@ -6,12 +8,20 @@ fun main() {
     val express = require("express")
     val app = express()
 
-    app.get("/") { req, res ->
-        res.type("text/plain")
-        res.send("i am a beautiful butterfly")
-    }
+    val request = require("request")
 
     app.use(express.static("public"))
+
+    app.get("/") { req, res ->
+        res.send("public")
+    }
+
+    app.get("/todos") { req, res ->
+        request.get(js("({url: 'https://jsonplaceholder.typicode.com/todos'})")) { err, response, body ->
+            res.type("text/json")
+            res.send(response.body)
+        }
+    }
 
     app.listen(3000) {
         println("Listening on port 3000")
